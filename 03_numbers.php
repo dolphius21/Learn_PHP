2ffe736aedeba20dd828f16db6f4162b0d44fc7c
dolphius21 <johnrudolphlee@gmail.com>
2021-07-15 15:41:18 +0800

    Done with numbers and strings lesson

--- 03_numbers.php
+++ 03_numbers.php
@@ -41,15 +41,15 @@ var_dump($number);
 echo '<br>';
 
 // Number functions
-echo "abs(-15) " . abs(-15) . '<br>';
-echo "pow(2,  3) " . pow(2, 3) . '<br>';
-echo "sqrt(16) " . sqrt(16) . '<br>';
-echo "max(2, 3) " . max(2, 3) . '<br>';
-echo "min(2, 3) " . min(2, 3) . '<br>';
-echo "round(2.4) " . round(2.4) . '<br>';
+echo "abs(-15) " . abs(-15) . '<br>'; // absolute value
+echo "pow(2,  3) " . pow(2, 3) . '<br>'; // exponential value
+echo "sqrt(16) " . sqrt(16) . '<br>'; // squareroot value
+echo "max(2, 3) " . max(2, 3) . '<br>'; // maximum value
+echo "min(2, 3) " . min(2, 3) . '<br>'; // minimum value
+echo "round(2.4) " . round(2.4) . '<br>'; // rounding number
 echo "round(2.6) " . round(2.6) . '<br>';
-echo "floor(2.6) " . floor(2.6) . '<br>';
-echo "ceil(2.4) " . ceil(2.4) . '<br>';
+echo "floor(2.6) " . floor(2.6) . '<br>'; // rounding down number
+echo "ceil(2.4) " . ceil(2.4) . '<br>'; // rounding up number
 
 // Formatting numbers
 $number = 123456789.12345;
