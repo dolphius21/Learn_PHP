8fa6fcfccdf0510d0a523406effdf2a8b03b875e
dolphius21 <johnrudolphlee@gmail.com>
2021-07-19 08:29:08 +0800

    Done with uploading image feature

--- 14_product_crud/create.php
+++ 14_product_crud/create.php
@@ -3,22 +3,65 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$errors = [];
+
+$title = '';
+$price = '';
+$description = '';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $title = $_POST['title'];
   $description = $_POST['description'];
   $price = $_POST['price'];
   $date = date('Y-m-d H:i:s');
 
-  $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
-    VALUES(:title, :image, :description, :price, :date)");
+  if (!$title) {
+    $errors[] = 'Product title is required.';
+  }
 
-  $statement->bindValue(':title', $title);
-  $statement->bindValue(':image', '');
-  $statement->bindValue(':description', $description);
-  $statement->bindValue(':price', $price);
-  $statement->bindValue(':date', $date);
-  $statement->execute();
+  if (!$price) {
+    $errors[] = 'Product price is required.';
+  }
+
+  if (!is_dir('images')) {
+    mkdir('images');
+  }
+
+  if (!$errors) {
+    $image = $_FILES['image'] ?? null;
+    $imagePath = '';
+
+    if ($image && $img['tmp_name']) {
+      $imagePath = 'images/' . randomString(8) . '/' . $image['name'];
+      mkdir(dirname($imagePath));
+
+      move_uploaded_file($image['tmp_name'], $imagePath);
+    }
+
+    $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+      VALUES(:title, :image, :description, :price, :date)");
+
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':image', $imagePath);
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':price', $price);
+    $statement->bindValue(':date', $date);
+    $statement->execute();
+
+    header('Location: index.php');
+  }
 };
+
+function randomString($n)
+{
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $str = '';
+  for ($i = 0; $i < $n; $i++) {
+    $index = rand(0, strlen($characters) - 1);
+    $str .= $characters[$index];
+  }
+  return $str;
+}
 
 ?>
 
@@ -39,7 +82,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
   <h1>Add New Product</h1>
 
-  <form action="" method="post">
+  <?php if (!empty($errors)) : ?>
+    <div class="alert alert-danger">
+      <?php foreach ($errors as $error) : ?>
+        <div><?php echo $error ?></div>
+      <?php endforeach; ?>
+    </div>
+  <?php endif; ?>
+
+  <form action="" method="post" enctype="multipart/form-data">
     <div class="mb-3">
       <label class="form-label">Product Image</label>
       <br />
@@ -47,15 +98,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
     <div class="mb-3">
       <label class="form-label">Product Title</label>
-      <input type="text" name="title" class="form-control">
+      <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
     </div>
     <div class="mb-3">
       <label class="form-label">Product Description</label>
-      <textarea class="form-control" name="description"></textarea>
+      <textarea class="form-control" name="description" value="<?php echo $description ?>"></textarea>
     </div>
     <div class="mb-3">
       <label class="form-label">Product Price</label>
-      <input type="number" step=".01" name="price" class="form-control">
+      <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
     </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
